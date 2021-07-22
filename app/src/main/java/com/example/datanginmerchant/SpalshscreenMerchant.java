146b755d6e79955c2c908073691706fe3789df15
M. Galih Pratama <mgalih.118140102@student.itera.ac.id>
2021-07-22 15:53:07 +0700

    UI Datangin role manager

---
+++ app/src/main/java/com/example/datanginmerchant/SpalshscreenMerchant.java
@@ -0,0 +1,14 @@
+package com.example.datanginmerchant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpalshscreenMerchant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalshscreen_merchant);
+    }
+}
---
+++ app/src/main/java/com/example/datanginmerchant/VerifikasiMerchant.java
@@ -0,0 +1,14 @@
+package com.example.datanginmerchant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifikasiMerchant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifikasi_merchant);
+    }
+}
